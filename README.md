3db19277271bf2477f8e28e0fd360eedcebc069c
AmeliaMargaurite <53086735+AmeliaMargaurite@users.noreply.github.com>
2022-08-13 20:47:59 +0200

    tester commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rummikub-server
+
 Heroku server for Rummikub - revisited
+
+Using the following tutorial as a starting point for multiplayer game
+https://medium.com/codesphere-cloud/getting-started-with-web-sockets-in-nodejs-d7088be88895
---
+++ server/index.js
@@ -0,0 +1,32 @@
+const WebSocket = require("ws");
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use("/", express.static(path.resolve(__dirname, "../client")));
+
+const myServer = app.listen(9876);
+
+const wsServer = new WebSocket.Server({
+	noServer: true,
+});
+
+wsServer.on("connection", function (ws) {
+	console.log("meep");
+	ws.on("message", function (msg) {
+		console.log(msg);
+		wsServer.clients.forEach(function each(client) {
+			if (client.readyState === WebSocket.OPEN) {
+				client.send(msg.toString());
+			}
+		});
+	});
+});
+
+myServer.on("upgrade", async function upgrade(request, socket, head) {
+	// math.random to reject half? maybe just a section to set up own reject scenario
+
+	wsServer.handleUpgrade(request, socket, head, function done(ws) {
+		wsServer.emit("connection", ws, request);
+	});
+});
